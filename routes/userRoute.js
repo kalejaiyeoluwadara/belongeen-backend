1aa74e390f5b592da357e7b72e6cadc270655785
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-23 22:51:19 +0100

    add user phone number, email, and password update functionalities

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -16,17 +16,22 @@ router.post("/reset-pass", userController.resetPassword);
 //Private Routes
 router.get("/saved-items", authMiddleware, userController.getSavedItems);
 router.get("/cart", authMiddleware, userController.getItemsInCart);
+router.post("/add_phone", authMiddleware, userController.addPhoneNumber);
+router.post("/change_email", authMiddleware, userController.changeEmail);
+router.post("/change_password", authMiddleware, userController.changePassword);
+
 router.patch(
   "/edit-profile",
   authMiddleware,
   userController.editAccountProfile
 );
-router.put("/cart", authMiddleware, userController.decrementCartItem);
+// update
+router.put("/cart", userController.decrementCartItem);
 router.delete("/saved-item", authMiddleware, userController.removeSavedItem);
-router.delete("/cart", authMiddleware, userController.removeItemFromCart);
-router.delete("/carts", authMiddleware, userController.clearCart);
+router.delete("/cart", userController.removeItemFromCart);
+router.delete("/carts", userController.clearCart);
 router.delete("/delete-account", authMiddleware, userController.deleteAccount);
-router.get("/:id", authMiddleware, userController.viewAccountProfile);
+router.get("/profile", authMiddleware, userController.viewAccountProfile);
 router.post("/save-item/:id", authMiddleware, userController.addSavedItem);
 router.post("/cart/:id", authMiddleware, userController.addItemToCart);
 
