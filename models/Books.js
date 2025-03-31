9480e17c277084b1fed7a2ee30016b6c74ec696f
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-03-31 22:24:58 +0100

    feat: add createdAt field to Book model for tracking creation date

--- models/Books.js
+++ models/Books.js
@@ -4,6 +4,9 @@ const BookSchema = new mongoose.Schema({
     name: { type: String},
     author: { type: String},
     image: { type: String},
+    createdAt: { type: Date, default: Date.now },
+
+
 });
 
 const Book = mongoose.model("Book", BookSchema);
